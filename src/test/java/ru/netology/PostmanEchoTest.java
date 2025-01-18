e74eab26a5b387dc1f9f3be1d0d9cdccd99f377b
Aleks Danilushkin <thenumber@ya.ru>
2025-01-18 16:18:23 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSentData() {
+        // Отправляем POST-запрос с телом "some data"
+        given()
+                .baseUri("https://postman-echo.com") // URL Postman Echo
+                .body("some data") // Тело запроса
+                .when()
+                .post("/post") // Конечная точка POST
+                .then()
+                .statusCode(200) // Проверяем, что статус ответа 200
+                .body("data", equalTo("some data")); // Проверяем, что тело ответа совпадает
+    }
+}
